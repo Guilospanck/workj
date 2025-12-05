a40405b66f3396f55159eaf4cdd507f64ec1739d
Guilherme Pereira <guilospanck@protonmail.com>
2025-12-05 17:38:47 +0000

    Add safeguards

--- scripts/git_worktree.sh
+++ scripts/git_worktree.sh
@@ -4,19 +4,16 @@ set -euo pipefail
 MAIN_BRANCH=origin/main
 WORKTREE_DIRECTORY="$PWD"
 
-get_or_create_worktree_directory(){
-	# Get/Create branch folder
-	local PROJECT_ROOT_LEVEL=$(git rev-parse --show-toplevel)
-	local PROJECT_NAME=$(git remote get-url origin | xargs basename -s .git)
-	WORKTREE_DIRECTORY="$PROJECT_ROOT_LEVEL/../${PROJECT_NAME}__worktrees/$BRANCH_NAME"
-	mkdir -p $WORKTREE_DIRECTORY
-}
-
-
 # Usage: add <branch_name>
 add(){
 	local BRANCH_NAME=$1
 	get_or_create_worktree_directory $BRANCH_NAME
+
+	# If worktreee already exists, do not proceed
+	if git worktree list --porcelain | grep -q "branch refs/heads/$BRANCH_NAME"; then
+	  echo $WORKTREE_DIRECTORY
+	  return
+	fi
 
 	if git rev-parse --verify $BRANCH_NAME >/dev/null 2>&1; then
 	  # Branch exists
@@ -32,6 +29,14 @@ add(){
 # Usage: remove <branch_name>
 remove(){
 	git worktree remove $1
+}
+
+get_or_create_worktree_directory(){
+	# Get/Create branch folder
+	local PROJECT_ROOT_LEVEL=$(git rev-parse --show-toplevel)
+	local PROJECT_NAME=$(git remote get-url origin | xargs basename -s .git)
+	WORKTREE_DIRECTORY="$PROJECT_ROOT_LEVEL/../${PROJECT_NAME}__worktrees/$BRANCH_NAME"
+	mkdir -p $WORKTREE_DIRECTORY
 }
 
 "$@"
--- scripts/workj.sh
+++ scripts/workj.sh
@@ -7,7 +7,12 @@ add() {
 	# gitworktree add
 	local WORKTREE_DIRECTORY=$("$PWD/scripts/git_worktree.sh" add $BRANCH_NAME)
 
-	"$PWD/scripts/zellij.sh" new_tab $BRANCH_NAME $WORKTREE_DIRECTORY
+	if [ -n "$WORKTREE_DIRECTORY" ]; then
+	  "$PWD/scripts/zellij.sh" new_tab $BRANCH_NAME $WORKTREE_DIRECTORY
+	else
+	  echo "WORKTREE_DIRECTORY is empty"
+	fi
+
 }
 
 remove() {
